b858a6b57eab68560039397db46acc266dd9651e
bilalbarina <dev.barina@gmail.com>
2023-01-17 14:58:56 +0100

    Implemented YouTube API

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,31 @@
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import config from "./config.json";
 
 function App() {
-    return (
-        <div className="">
-            
-        </div>
-    );
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    axios.get(config.YOUTUBE_BASE_URI, {
+      params: {
+        part: "snippet",
+        q: "tanger",
+        key: config.YOUTUBE_API_KEY,
+      },
+    }).then((response) => {
+      setVideos(response.data.items)
+    });
+  }, []);
+
+  return (
+    <div className="">
+      <ul>
+        {videos.map((video) => (
+          <li> {video.snippet.title} </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
